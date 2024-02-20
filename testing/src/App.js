c1bbb322008e864bb97cbe7c91a9454ac6348129
dunuraHansaja <dunuhansaja@gmail.com>
2024-02-20 22:49:44 +0530

    Edited some changes in crop guidance front end page
    Did some changes to the Crop guidance front end page. Added some parts to the css main page.

--- testing/src/App.js
+++ testing/src/App.js
@@ -1,44 +1,61 @@
 import React from 'react';
-import './App.css'; // Import your CSS file for styling
+import './App.css';
+
+
 
 function App() {
   return (
     <div>
-      <h1>Crop Guidance</h1>
+      <h1> <span id='crop'>Crop</span> Guidance</h1>
+      <div className='padding_1'>
+      {/* <h4>Select your preferd crop</h4> */}
+      </div>
+      
       <div className="container">
         <div className="row">
           <a href='https://www.youtube.com/'>
           <div className="imageBox">
             <img src="rice_cg.jpg" alt="placeholder" />
-            <div className="textBox">Rice Crop</div>
+            <div className="textBox">Rice Crop
+              <div className='textBox_1'>Read More...</div>
+            </div>
+            
           </div>
           </a>
 
-          <a>
+          <a href='https://www.youtube.com/'>
           <div className="imageBox">
             <img src="big_onion_cg.jpg" alt="placeholder" />
-            <div className="textBox">Big Onion Yield</div>
+            <div className="textBox">Big Onion Yield
+              <div className='textBox_1'>Read More...</div>
+            </div>
           </div>
           </a>
-          <a>
+          <a href='https://www.youtube.com/'>
           <div className="imageBox">
             <img src="maize_cg.jpg" alt="placeholder" />
-            <div className="textBox">Maize(Corn) Cultivation</div>
+            <div className="textBox">Maize(Corn) Cultivation
+              <div className='textBox_1'>Read more...</div>
+            </div>
           </div>
           </a>
           
         </div>
         <div className="row">
-          <a>
+        <a href='https://www.youtube.com/'>
           <div className="imageBox">
             <img src="finger_millet_cg.jpg" alt="placeholder" />
-            <div className="textBox">Finger Millet Cultivation</div>
+            <div className="textBox">Finger Millet Cultivation
+              <div className='textBox_1'>Read more...</div>
+            </div>
           </div>
           </a>
-          <a>
+          <a href='https://www.youtube.com/'>
           <div className="imageBox">
             <img src="Chilli_cg.jpg" alt="placeholder" />
-            <div className="textBox">Chilli Cultivation</div>
+            <div className="textBox">Chilli Cultivation
+            <div className='textBox_1'>Read more...</div>
+            </div>
           </div>
           </a>
         </div>
